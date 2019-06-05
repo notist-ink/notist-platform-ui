d7bc10a9141ff701276f7aa27bdc05ec72677424
Matthew Davis <matthew@matthewdavis.io>
2019-06-05 18:22:09 -0500

    components

---
+++ src/app/_layout/left-menu-sidebar/left-menu-sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-left-menu-sidebar',
+  templateUrl: './left-menu-sidebar.component.html',
+  styleUrls: ['./left-menu-sidebar.component.scss']
+})
+export class LeftMenuSidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,74 @@
-import { NgModule }        from '@angular/core';
-import { BrowserModule }   from '@angular/platform-browser';
-import { QuillModule }     from 'ngx-quill';
-import { HeaderComponent } from './_layout/header/header.component';
+import { NgModule }                 from '@angular/core';
+import { MatTooltipModule }         from '@angular/material';
+import { BrowserModule }            from '@angular/platform-browser';
+import { BrowserAnimationsModule }  from '@angular/platform-browser/animations';
+import { RouterModule }             from '@angular/router';
+import { QuillModule }              from 'ngx-quill';
+import { HeaderComponent }          from './_layout/header/header.component';
+import { LeftMenuSidebarComponent } from './_layout/left-menu-sidebar/left-menu-sidebar.component';
+import { SidebarComponent }         from './_layout/sidebar/sidebar.component';
 
-import { AppComponent } from './app.component';
-import { SidebarComponent } from './_layout/sidebar/sidebar.component';
+import { AppComponent }      from './app.component';
+import { BooksComponent }    from './books/books.component';
+import { EditorComponent }   from './editor/editor.component';
+import { HomeComponent }     from './home/home.component';
+import { NotesComponent }    from './notes/notes.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
 
     declarations: [
 
         AppComponent,
-
+        EditorComponent,
         HeaderComponent,
-
-        SidebarComponent
+        HomeComponent,
+        SidebarComponent,
+        LeftMenuSidebarComponent,
+        SettingsComponent,
+        NotesComponent,
+        BooksComponent
 
     ],
 
     imports: [
 
         BrowserModule,
-        QuillModule
+        BrowserAnimationsModule,
+        MatTooltipModule,
+        QuillModule,
+
+        RouterModule.forRoot([
+
+            {
+
+                path: 'books',
+                component: BooksComponent
+
+            }, {
+
+                path: 'home',
+                component: HomeComponent
+
+            }, {
+
+                path: 'notes',
+                component: NotesComponent
+
+            }, {
+
+                path: 'settings',
+                component: SettingsComponent
+
+            }, {
+
+                path: '',
+                pathMatch: 'full',
+                redirectTo: 'home'
+
+            }
+
+        ])
 
     ],
 
